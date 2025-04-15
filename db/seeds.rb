ea14e292214f5a428a2bafe1a8802fa55bfd3682
Adnan <adnanneyazi88@gmail.com>
2025-04-15 18:44:03 +0530

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,4 @@
-User.create!(email: "adnanneyazi88@gmail.com", password: "password", role: "admin")
+User.find_or_create_by!(email: "admin@example.com") do |user|
+  user.password = "password"
+  user.name = "Admin"
+end
